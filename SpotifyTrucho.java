09a759e681e4ce514dfd36cf5429d96693196b89
Benjamin Tapia <aguilar.tapia.benjamin@gmail.com>
2023-08-19 10:04:50 -0400

    Primera clase

---
+++ SpotifyTrucho.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package cl.duoc.pgy2121.SpotifyTrucho;
+
+/**
+ *
+ * @author Cetecom
+ */
+public class SpotifyTrucho {
+
+    public static void main(String[] args) {
+        char sexo = 'M';
+        String nombre = "Benjamin";
+        boolean isValid = false;
+        byte bytes;
+        int edad = 19;
+        float decimal = 3.10f;
+        double doble = 3.10d;
+        long numerolargo = 121212112;
+        short numerochiquito = 34;
+         
+        System.out.println("Nombre: " + nombre);  
+        System.out.println("Edad: " + edad);
+        System.out.println("Activo: " + isValid);
+    }
+}
